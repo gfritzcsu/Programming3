a0d19af6dea7ca8bed842a52a8ce79e0a8dc49b2
gfritz <gregory.fritz@csuglobal.edu>
2025-12-14 12:18:57 -0700

    adds mod1 critical thinking assignment

---
+++ Mod1CritThinking/test/src/test.cpp
@@ -0,0 +1,34 @@
+//============================================================================
+// -------------------------------------------
+// Program Name: C++ Console output test
+// Author: Greg Fritz
+// Professor Herbert Pensado
+// CSC-450-1 Programming 3
+// Date: December 14, 2025
+// -------------------------------------------
+//============================================================================
+// Pseudocode:
+//BEGIN MAIN
+//
+//    OUTPUT "First Name: Greg"
+//    OUTPUT "Last Name: Fritz"
+//    OUTPUT "Street Address: 123 Street Address"
+//    OUTPUT "City and State: Aurora, CO"
+//    OUTPUT "Zip Code: 80018"
+//
+//END MAIN
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // Display fictional person's information
+    cout << "First Name: Greg" << endl;
+    cout << "Last Name: Fritz" << endl;
+    cout << "Street Address: 123 Street Address" << endl;
+    cout << "City and State: Aurora, CO" << endl;
+    cout << "Zip Code: 80018" << endl;
+
+    return 0;
+}
